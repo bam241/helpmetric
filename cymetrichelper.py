2f94f47c8b2efa23996f9b2bff2c456de3d94e26
Mouginot B <mouginot.baptiste@gmail.com>
2018-03-12 08:53:37 -0500

    some more cymetric related functions

--- cymetrichelper.py
+++ cymetrichelper.py
@@ -2,7 +2,15 @@
 import cymetric as cym
 from cymetric import graphs as cgr
 from cymetric import timeseries as tm
+import pandas as pd
 
+
+def Trans(file, rec=(), send=(), nucs_=(), coms=()):
+    db = cym.dbopen(file)
+    ev = cym.Evaluator(db=db, write=False)
+    df1 = tm.transactions(ev, receivers=rec, senders=send,
+                          nucs=nucs_, commodities=coms)
+    return df1
 
 
 def InvFrac(file, facility, nucs1=[''], nucs2=[''], factor1=1, factor2=1):
@@ -31,3 +39,49 @@ def MakeFlowGraph(file, label=''):
     db_ = cym.dbopen(file)
     ev_ = cym.Evaluator(db=db_, write=False)
     return cgr.flow_graph(evaler=ev_, label=label)
+
+
+# mode 0 non cumulative
+# mode 1 cumul
+# mode 2 mean
+def month2year(df, mode=0, division=12):
+    dfn = pd.DataFrame(columns=['Time', 'Mass'])
+    df.rename(index=str, columns={"Quantity": "Mass"})
+    val = 0
+    for index, row in df.iterrows():
+        if mode == 0:
+            val = row['Mass']
+        else:
+            val += row['Mass']
+        if row['Time'] % division == 0:
+            if mode == 2:
+                val *= 1. / float(division)
+            dfn.loc[int(row['Time'] / division)] = int(row['Time'] / 12)
+            dfn.loc[int(row['Time'] / division)]['Mass'] = val
+            val = 0
+    return dfn
+
+
+def maxperdiv(df, division=12):
+    dfn = pd.DataFrame(columns=['Time', 'Mass'])
+    val = 0
+    for index, row in df.iterrows():
+        if val < row['Mass']:
+            val = row['Mass']
+        if row['Time'] % division == 0:
+            dfn.loc[int(row['Time'] / division)] = int(row['Time'] / 12)
+            dfn.loc[int(row['Time'] / division)]['Mass'] = val
+            val = 0
+    return dfn
+
+
+def increaseonly(df):
+    dfn = pd.DataFrame(columns=['Time', 'Mass'])
+    val = 0
+    for index, row in df.iterrows():
+        if val < row['Mass']:
+            val = row['Mass']
+            dfn.loc[int(row['Time'])] = int(row['Time'])
+            dfn.loc[int(row['Time'])]['Mass'] = row['Mass']
+            val = row['Mass']
+    return dfn
